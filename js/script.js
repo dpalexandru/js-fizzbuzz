8906ec67b4f59eefea01d3f4077d706707e04cff
dpalexandru <alexpoian91@hotmail.com>
2025-07-02 15:31:57 +0200

    [add]filtred numbers divisible by 3 and 5

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 //Costruisco un ciclo che stampa i numeri da 1 a 100
 
 for (let i = 1; i<= 100 ; i++) {
-  console.log(i);
-}
+  //aggiungo if per selezionare i numeri divisibili sia per 3 che per 5 
+  if (i %3 == 0 && i %5 ==0) {
+    console.log(`Fizzbuzzzzzz`)
+  }
+  else {
+    console.log(i);
+}}
